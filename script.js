4a09db1f34a5f4d748e7274057260fc2391b7c66
Eric Grimes <erictgrimes@gmail.com>
2025-06-08 10:18:33 -0700

    rendered all puppies to the page.

--- script.js
+++ script.js
@@ -5,17 +5,73 @@ const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${COHORT}/players`;
 
 // place holder for players
 const state = {
-  players: [],
+  puppies: [],
+  currentPuppy: {},
+  puppyId: {},
 };
 
 //fetch all players from api
-const fetchAllPlayers = async () => {
+const fetchAllPuppies = async () => {
   try {
     const response = await fetch(API_URL);
     const { data } = await response.json();
-    state.players = data.players;
-    renderPlayers();
+    state.puppies = data.players;
+    renderPuppies();
   } catch (error) {
     console.log(error);
   }
 };
+
+//render puppies
+const renderPuppies = () => {
+  const puppiesContainer = document.getElementById("puppy-list-container");
+  const puppyList = state.puppies;
+
+  //checks if there are puppies to display
+  if (puppyList.length === 0 || !puppyList) {
+    puppiesContainer.innerHTML = "<p>No puppies available</p>";
+    return;
+  }
+
+  puppyList.forEach((puppy) => {
+    const puppyListCard = document.createElement("div");
+    puppyListCard.classList.add("puppy-list-card");
+    puppyListCard.innerHTML = `
+      <button><strong>${puppy.name}</strong></button>`;
+
+       puppiesContainer.appendChild(puppyListCard);
+
+  });
+
+  
+ 
+};
+
+const getPuppyData = async (puppy) => {
+  try {
+
+    const response = await fetch(`${API_URL}/${puppy.id}`);
+    const json = await response.json();
+
+    const data =  {
+
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+
+
+
+
+
+
+//initialize the app
+const init = async () => {
+  await fetchAllPuppies();
+};
+
+
+init();
